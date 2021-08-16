468a970dc38d664c24366aac4aa8f87c2220902a
z-memory <dont@forgetmy.email>
2021-08-16 20:32:37 +0200

    uhuhh

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 5000;
+import path from 'path';
+
+// DRY 
+// DONT REPEAT YOURSELF
+
+app.set('view engine', 'pug')
+app.use(express.json());
+
+SSR
+SERVER SIDE RENDERING
+
+app
+    .route('/')
+    .get((request, response) => {
+        response.render('index', { title: 'Hey', message: 'Hello there!', name: 'bob' })
+        // response.sendFile(path.join(process.cwd(),'index.html'))
+    })
+    
+app
+    .route('/about')
+    .get((request, response) => {
+        response.render('about', { message: 'this is the about page i guess!'})
+    })
+    
+app
+    .route('/messages')
+    .get((request, response) => {
+        response.json(['Hello', 'World'])
+    })
+    .post((request, response) => {
+        console.log('messages post request received!', request.body)
+        response.send('messages post request received!')
+    })
+    .put((request, response) => {
+        console.log('messages put request received!')
+        response.send('messages put request received!')
+    })
+    .delete((request, response) => {
+        console.log('messages delete request received!')
+        response.send('messages delete request received!')
+    });
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
